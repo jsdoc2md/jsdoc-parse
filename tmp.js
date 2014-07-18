11d18e2c791640ff5dc16bc400c343be47dfb396
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-18 17:09:20 +0100

    tmp check

--- tmp.js
+++ tmp.js
@@ -1,9 +1,11 @@
 var cp = require("child_process");
 
-cp.exec("jsdoc", function(err, stdout, stderr){
+cp.exec("./node_modules/.bin/jsdoc lib/jsdoc-parse.js", function(err, stdout, stderr){
     console.log("Failed: ", err);
     console.log("stdout: " + stdout.length);
     console.log("stderr: " + stderr.length);
+
+    console.log(stdout);
 });
 
 // cp.spawn("jsdoc", [], { stdio: "inherit" });
