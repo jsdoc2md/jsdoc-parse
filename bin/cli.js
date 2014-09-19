b74d4842c81fc97509a944ea71816cbc4d6a7a6d
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-09-19 18:18:54 +0100

    error handlin, refactor

--- bin/cli.js
+++ bin/cli.js
@@ -7,10 +7,10 @@ var files = process.argv.slice(2);
 if (files.length){
     var parseStream = parse(files);
     parseStream
-        .on("error", console.log)
+        .on("error", console.error)
         .pipe(process.stdout);
 } else {
     var parseStream = parse();
-    parseStream.on("error", console.log);
+    parseStream.on("error", console.error);
     process.stdin.pipe(parseStream).pipe(process.stdout);
 }
