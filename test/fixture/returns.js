40b7927132a1751daae3fb095dc3437450f3a549
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-09-26 08:56:02 +0100

    wip

---
+++ test/fixture/returns.js
@@ -0,0 +1,6 @@
+/**
+a global function returning multiple stuff
+@returns {string} if something you get a string back
+@returns {object|function} or an object, or function
+*/
+function globalFunction(one, two){}
