f863baee0edc4d4f792bcdcbf9202130ca889a72
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-09-29 23:39:27 +0100

    custom tags

--- test/fixture/custom.js
+++ test/fixture/custom.js
@@ -1,7 +1,7 @@
 /**
 a global var with custom tags
 @sitcom The Office
-@character Rodney Trotter
+@character Oggmeister
 */
 var customTagged = true;
 
@@ -9,7 +9,15 @@ var customTagged = true;
 the constructor
 @class
 @classdesc a class that has custom tags
-@sitcom The Office
+@sitcom Only Fools
 @character Rodney Trotter
 */
 function customClass(){}
+
+/**
+a global function with custom tags
+@sitcom Brass Eye
+@character Noel Edmonds
+*/
+function customFunction(){}
+
