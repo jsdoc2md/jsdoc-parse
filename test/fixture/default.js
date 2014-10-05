4903762cfa9fa964bfc4a35aa5e8c4d3e26dd430
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-10-05 13:44:58 +0100

    wip

---
+++ test/fixture/default.js
@@ -0,0 +1,11 @@
+/**
+@type {object}
+@default { one: 1, two: "yeah?" }
+*/
+var defaultedOne = {};
+
+/**
+@type {function}
+@default function f(n){ return something(n); }
+*/
+var defaultedTwo = {};
