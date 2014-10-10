65adabf1f66501bae57f7ba0687b5c841570592b
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-10-10 10:59:31 +0200

    wip

---
+++ test/fixture/private.js
@@ -0,0 +1,12 @@
+/** @namespace */
+var Documents = {
+    /**
+     * An ordinary newspaper.
+     */
+    Newspaper: 1,
+    /**
+     * My diary.
+     * @private
+     */
+    Diary: 2
+};
--- test/fixture/todo.js
+++ test/fixture/todo.js
@@ -1,7 +1,19 @@
 /**
  * @todo Write the documentation.
  * @todo Implement this function.
+ */
+function todoFunction1() {}
+
+/**
+ * @todo Write the documentation.
+ * @todo Implement this function.
  * @done this one is done
  * @done finished
  */
-function todoFunction() {}
+function todoFunction2() {}
+
+/**
+ * @done this one is done
+ * @done finished
+ */
+function todoFunction3() {}
