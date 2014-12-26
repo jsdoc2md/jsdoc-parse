0cf30f51e2ad7b8843b19929ed5aa91ff29911ca
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-12-26 18:23:07 +0000

    fix for 75lb/jsdoc-to-markdown#14. Jsdoc-parse now respects @ignore in stream mode

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,18 @@ var parse = require("../");
 
 var files = process.argv.slice(2);
 if (files.length){
-    parse(files).pipe(process.stdout);
+    parse(files)
+        .on("error", function(err){
+            console.error("ERROR: ", err);
+            process.exit(1);
+        })
+        .pipe(process.stdout);
 } else {
-    process.stdin.pipe(parse()).pipe(process.stdout);
+    process.stdin
+        .pipe(parse())
+        .on("error", function(err){
+            console.error("ERROR: ", err);
+            process.exit(1);
+        })
+        .pipe(process.stdout);
 }
