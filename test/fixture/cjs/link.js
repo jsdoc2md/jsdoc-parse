df2c14e8d396389d4c199b8f6d927a6627e66586
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-12-27 00:00:39 +0000

    link case

---
+++ test/fixture/cjs/link.js
@@ -0,0 +1,25 @@
+/** 
+A module. Refer to it using {@link module:linkMod|this link}.
+@module linkMod
+*/
+/** 
+The built in Math object. Refer to it with {@link external:Math}.
+@external Math
+@see http://www.math.com
+*/
+/** 
+The built in Date object, it has no see tag. Refer to it with {@link external:Date}.
+@external Date
+*/
+/** 
+An event. Refer to with {@link module:linkMod.event:MyEvent this link}.
+@event module:linkMod.event:MyEvent
+*/
+
+/**
+this is from {@link module:linkMod}.. it also references {@link external:Math}
+@class
+@extends external:Math
+@emits module:linkMod.event:MyEvent
+*/
+function linksYeah(){}
--- test/fixture/link.js
+++
@@ -1,9 +0,0 @@
-/** A module. Refer to it using {@link module:foo/bar}.
- * @module linkMod
- */
-/** The built in Math object. Refer to it with {@link external:Math}.
- * @external Math
- */
-/** An event. Refer to with {@link module:linkMod.event:MyEvent}.
- * @event module:linkMod.event:MyEvent
- */
