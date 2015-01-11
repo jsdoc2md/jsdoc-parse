7e272da00b1aba4a994537965bd8ab34b1c53cfb
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-01-11 00:28:44 +0000

    enum parents

--- test/fixture/function.js
+++ test/fixture/function.js
@@ -2,5 +2,7 @@
 a global function
 @param {number} - first param
 @param {string} - second param
+@param {number|string} - third
+@param {Array.<Number|String>} - fourth
 */
-function globalFunction(one, two){}
+function globalFunction(one, two, three, four){}
