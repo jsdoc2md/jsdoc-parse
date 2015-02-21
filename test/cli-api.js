d654cf91f6734753089279b9eba25f81dc893d37
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-02-21 23:44:15 +0000

    sort-by, tests

--- test/cli-api.js
+++ test/cli-api.js
@@ -14,25 +14,25 @@ try{
 
 test("valid json out", function(t){
     t.plan(1);
-    var stream = parse("lib/jsdoc-parse.js");
+    var stream = parse("test/fixture/chainable.js");
     stream.on("readable", function(){
         var chunk = this.read();
         if (chunk){
             var data = JSON.parse(chunk);
-            t.equal(data[0].name, "jsdoc-parse");
+            t.equal(data[0].name, "Chainable");
         }
     });
 });
 
 test("stdin input working", function(t){
     t.plan(1);
-    var inputFile = fs.openSync("lib/jsdoc-parse.js", "r");
+    var inputFile = fs.openSync("test/fixture/chainable.js", "r");
     var outputFile = fs.openSync("tmp/jsdoc-parse.json", "w");
     var handle = spawn("node", [ "bin/cli.js"], { stdio: [ inputFile, outputFile, process.stderr ]});
     handle.on("close", function(){
         var json = fs.readFileSync("tmp/jsdoc-parse.json", "utf8");
         var data = JSON.parse(json);
-        t.equal(data[0].name, "jsdoc-parse");
+        t.equal(data[0].name, "Chainable");
     });
 });
 
@@ -43,8 +43,8 @@ test("glob expression", function(t){
         var chunk = this.read();
         if (chunk){
             var data = JSON.parse(chunk);
-            t.equal(data[0].name, "jsdoc-parse");
-            t.equal(data[2].name, "publish");
+            t.ok(data[0].name);
+            t.ok(data[2].longname);
         }
     });
 });
