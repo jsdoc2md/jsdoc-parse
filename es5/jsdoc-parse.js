8d992ff93f0bc947727537e80d489ce351ddcf4c
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-08-03 23:24:42 +0100

    moved private option from jsdoc-parse to dmd

--- es5/jsdoc-parse.js
+++ es5/jsdoc-parse.js
@@ -8,19 +8,6 @@ function parse(jsdocData, options) {
   options = options || {};
   var data = transform(jsdocData);
 
-  data = data.filter(function (doclet) {
-    var parent = data.find(function (d) {
-      return d.id === doclet.memberof;
-    }) || {};
-    if (doclet.ignore || parent.ignore) {
-      return false;
-    } else if (!options.private && (doclet.access === 'private' || parent.access === 'private')) {
-      return false;
-    } else {
-      return true;
-    }
-  });
-
   data = sort(data, options['sort-by']);
   return data;
 }
