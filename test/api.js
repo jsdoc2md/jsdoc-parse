04c63da199132b8fa846ebc234347f5be922a79f
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-10-04 22:25:28 +0100

    upgrade deps.. docs

--- test/api.js
+++ test/api.js
@@ -1,10 +1,11 @@
 'use strict'
-var test = require('test-runner')
+var TestRunner = require('test-runner')
 var jsdocParse = require('../')
 var fs = require('fs')
 var a = require('assert')
+var runner = new TestRunner()
 
-test('api: valid json out', function (t) {
+runner.test('api: valid json out', function () {
   var jsdocOutput = JSON.parse(fs.readFileSync('./test/fixture/jsdoc-input.json', 'utf8'))
   var data = jsdocParse(jsdocOutput)
   a.strictEqual(data[0].name, 'Chainable')
