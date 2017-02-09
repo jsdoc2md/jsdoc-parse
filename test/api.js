d6ff9e30fe656e11ad98b2ad62667acdd50462f9
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-02-09 20:24:30 +0000

    remove support for node < v4.0.0

--- test/api.js
+++ test/api.js
@@ -1,12 +1,12 @@
 'use strict'
-var TestRunner = require('test-runner')
-var jsdocParse = require('../')
-var fs = require('fs')
-var a = require('assert')
-var runner = new TestRunner()
+const TestRunner = require('test-runner')
+const jsdocParse = require('../')
+const fs = require('fs')
+const a = require('assert')
+const runner = new TestRunner()
 
 runner.test('api: valid json out', function () {
-  var jsdocOutput = JSON.parse(fs.readFileSync('./test/fixture/jsdoc-input.json', 'utf8'))
-  var data = jsdocParse(jsdocOutput)
+  const jsdocOutput = JSON.parse(fs.readFileSync('./test/fixture/jsdoc-input.json', 'utf8'))
+  const data = jsdocParse(jsdocOutput)
   a.strictEqual(data[0].name, 'Chainable')
 })
